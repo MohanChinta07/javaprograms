3a604122e0756804867c615f672e1f5e5af39c2d
MohanChinta07 <137123404+MohanChinta07@users.noreply.github.com>
2024-01-17 22:15:05 -0600

    New program

---
+++ Percentage.java
@@ -0,0 +1,17 @@
+public class Percentage {
+    public static void main(String[] args){
+        int a= 78;
+        int b= 45;
+        int c= 62;
+        float Robert_Marks = a+b+c;
+        float Robert_Per = (Robert_Marks/300) * 100;
+        System.out.println("Total marks for 300 is:");
+        System.out.println(Robert_Marks);
+        System.out.println("Percentage of Robert");
+        System.out.println(Robert_Per);
+
+
+
+
+    }
+}
