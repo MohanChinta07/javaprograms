aeabda3dac61232d59cf7ec9ca2a1296a5837cf4
MohanChinta07 <137123404+MohanChinta07@users.noreply.github.com>
2024-01-21 13:28:49 -0600

    Methods

---
+++ Animal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Animal {
+        String col;
+        String Sound;
+        String Name;
+        int age;
+
+        public void displayAnimal_info(){
+                System.out.println(col);
+                System.out.println(Sound);
+                System.out.println(Name);
+                System.out.println(age);
+
+        }
+        public static void main(String[] args){
+                //Animal_info ani = new Animal();
+
+
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+        int empid;
+        String ename;
+        String role;
+        float salary;
+
+        void dispalyEmployeeInfo() {
+            System.out.println(empid);
+            System.out.println(role);
+            System.out.println(ename);
+            System.out.println(salary);
+        }
+
+        public static void main(String []args) {
+
+        }
+}
